230d4d9c13ae8e3c386d3a39fe48ce8c42acf096
Plínio Mayer <pctmayer@gmail.com>
2025-04-07 13:58:12 -0300

    feat: add tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import { PaperProvider, MD3LightTheme } from 'react-native-paper';
+import { Tabs } from 'expo-router';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function RootLayout() {
-  return <Stack />;
+  return <PaperProvider>
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="objetivos"
+        options={{
+          title: 'Objetivos',
+          tabBarActiveTintColor: MD3LightTheme.colors.primary,
+          tabBarIcon: ({ color }: { color: string }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="plinios"
+        options={{
+          title: 'Plínios',
+          tabBarActiveTintColor: MD3LightTheme.colors.primary,
+          tabBarIcon: ({ color }: { color: string }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+    </Tabs>
+  </PaperProvider>;
 }
