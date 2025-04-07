d059f5c4ac842516595f7c5f4e41f316e00626cb
Plínio Mayer <pctmayer@gmail.com>
2025-04-07 19:20:48 -0300

    feat: prettier and eslint

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,32 @@
-import { PaperProvider, MD3LightTheme } from 'react-native-paper';
-import { Tabs } from 'expo-router';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { PaperProvider, MD3LightTheme } from "react-native-paper";
+import { Tabs } from "expo-router";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function RootLayout() {
-  return <PaperProvider>
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen
-        name="objetivos"
-        options={{
-          title: 'Objetivos',
-          tabBarActiveTintColor: MD3LightTheme.colors.primary,
-          tabBarIcon: ({ color }: { color: string }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="plinios"
-        options={{
-          title: 'Plínios',
-          tabBarActiveTintColor: MD3LightTheme.colors.primary,
-          tabBarIcon: ({ color }: { color: string }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
-      />
-    </Tabs>
-  </PaperProvider>;
+  return (
+    <PaperProvider>
+      <Tabs screenOptions={{ headerShown: false }}>
+        <Tabs.Screen
+          name="objetivos"
+          options={{
+            title: "Objetivos",
+            tabBarActiveTintColor: MD3LightTheme.colors.primary,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <FontAwesome size={28} name="home" color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="plinios"
+          options={{
+            title: "Plínios",
+            tabBarActiveTintColor: MD3LightTheme.colors.primary,
+            tabBarIcon: ({ color }: { color: string }) => (
+              <FontAwesome size={28} name="home" color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </PaperProvider>
+  );
 }
