2284a43ea0686642a5caabeb58465b97eb1df302
Plínio Mayer <pctmayer@gmail.com>
2025-04-09 18:57:08 -0300

    fix: CRLF

--- components/contador.component.tsx
+++ components/contador.component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { StyleSheet, View, Text, ViewStyle } from "react-native";
 
 const styles = StyleSheet.create({
